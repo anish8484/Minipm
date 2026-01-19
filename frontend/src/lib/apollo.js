e73509f44ce2fc7dc3e430fc0a1840150d09114e
anish8484 <anish8484@users.noreply.github.com>
2026-01-20 00:47:51 +0530

    Create apollo.js

---
+++ frontend/src/lib/apollo.js
@@ -0,0 +1,78 @@
+import { ApolloClient, InMemoryCache, createHttpLink, split } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const HTTP_URL = `${BACKEND_URL}/api/graphql`;
+const WS_URL = HTTP_URL.replace('https://', 'wss://').replace('http://', 'ws://');
+
+const httpLink = createHttpLink({
+  uri: HTTP_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// WebSocket link for subscriptions
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: WS_URL,
+    connectionParams: () => {
+      const token = localStorage.getItem('token');
+      return {
+        authorization: token ? `Bearer ${token}` : '',
+      };
+    },
+  })
+);
+
+// Split link to route subscriptions to WebSocket
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+export const apolloClient = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          projects: {
+            merge(existing = [], incoming) {
+              return incoming;
+            },
+          },
+          tasks: {
+            merge(existing = [], incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
+
+export default apolloClient;
